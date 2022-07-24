d66987fe14572046d64ac03c9ebdc0139d67436e
Ulyana Kalinichenko <ulyakalinichenko1990@gmail.com>
2022-07-24 16:42:15 +0300

    task -02js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+for (let i = 0; i < ingredients.length; i += 1){
+  const elementLi = document.createElement('li');
+  elementLi.classList.add('item');
+  elementLi.textContent = ingredients[i];
+  console.log(elementLi);
+
+  const elementUl = document.querySelector('ul#ingredients');
+  elementUl.appendChild(elementLi);
+}
