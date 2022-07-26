3926da3c0900586037c028b41efd7113c82d6d94
Ulyana Kalinichenko <ulyakalinichenko1990@gmail.com>
2022-07-26 15:02:20 +0300

    task-09js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector(".change-color");
+const bodyColor = document.querySelector('body');
+
+button.addEventListener("click", () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  const buttonColorText = document.querySelector(".color");
+  buttonColorText.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+});
+
+
